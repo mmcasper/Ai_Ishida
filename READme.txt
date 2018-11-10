dcfb5f8b9ce6623c4ed9af6f98a3082fed02c2d1
mmcasper <casper1974@gmail.com>
2018-11-10 17:38:09 -0500

    fresh files for github project page

---
+++ READme.txt
@@ -0,0 +1,5 @@
+This Webpage is a project to fufill the Front End Web Development cohort for Code Louisville, fall 2018.
+
+This simple page targets various career elements of a classical musician and includes photos, audio and video.
+
+It is built primarily with HTML and CSS with some required javaScript. Bootstrap is primarily used for responsive layout.
---
+++ app.js
@@ -0,0 +1,36 @@
+
+// document.getElementById("lessons").onmouseover = function() {mouseOver()};
+// document.getElementById("lessons").onmouseout = function() {mouseOut()};
+
+// function mouseOver() {
+//     document.getElementById("lessons").style.color = "purple";
+// }
+
+// function mouseOut() {
+//     document.getElementById("lessons").style.color = "white";
+// }
+
+// document.getElementById("elmhurstLink").onmouseover = function() {mouseOver()};
+// document.getElementById("elmhurstLink").onmouseout = function() {mouseOut()};
+
+// function mouseOver() {
+//     document.getElementById("elmhurstLink").style.color = "purple";
+// }
+
+// function mouseOut() {
+//     document.getElementById("elmhurstLink").style.color = "white";
+// }
+
+// const secureLinks=$("a['href^=https://']");
+// $secureLinks.attr("target","_blank");
+
+// $('a[href^="https://"]').attr('target','_blank');
+$("a[href^='http']").attr('target','_blank');
+
+// $('a[href^="https://"]').attr('target', '_blank');
+
+
+function myFunction() {
+    alert('Pricing Coming Soon!');
+}
+
