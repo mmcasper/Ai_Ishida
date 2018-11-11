8afd2ccf242aec265f84f531580efd234d357957
mmcasper <casper1974@gmail.com>
2018-11-11 04:58:37 -0500

    final except images and some layout

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-
-// document.getElementById("lessons").onmouseover = function() {mouseOver()};
-// document.getElementById("lessons").onmouseout = function() {mouseOut()};
-
-// function mouseOver() {
-//     document.getElementById("lessons").style.color = "purple";
-// }
-
-// function mouseOut() {
-//     document.getElementById("lessons").style.color = "white";
-// }
-
-// document.getElementById("elmhurstLink").onmouseover = function() {mouseOver()};
-// document.getElementById("elmhurstLink").onmouseout = function() {mouseOut()};
-
-// function mouseOver() {
-//     document.getElementById("elmhurstLink").style.color = "purple";
-// }
-
-// function mouseOut() {
-//     document.getElementById("elmhurstLink").style.color = "white";
-// }
-
-// const secureLinks=$("a['href^=https://']");
-// $secureLinks.attr("target","_blank");
-
-// $('a[href^="https://"]').attr('target','_blank');
-
 const secureLinks=$('a[href^="http"]');
 $(secureLinks).attr('target','_blank');
 
