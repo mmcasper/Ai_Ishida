e2856f6d2a464705580893573498f2ce12548104
mmcasper <casper1974@gmail.com>
2018-11-12 19:42:03 -0500

    created folders, changed paths

---
+++ JS/app.js
@@ -0,0 +1,16 @@
+
+// ---alternate version to target_blank all external links---//
+    // const secureLinks=$('a[href^="http"]');
+    // $(secureLinks).attr('target','_blank');
+
+    // -----jquery to target_blank all external links.----//
+$('a[href^="http"]').attr('target', '_blank');
+
+// ----pop up as place holder for pricing list-----//
+function myFunction() {
+    alert('Pricing Coming Soon!');
+}
+
+
+
+
